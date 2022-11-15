7cd576531ef5053208686bb7a496df66981501a3
bsravani <bsravani@bsravani-lp.pkrmsezdc.com>
2022-11-15 18:13:49 +0530

    mobilerecharge

---
+++ Mobile-Recharge-project/src/main/java/com/MobileRecharge/MobileRechargeproject/MobileRechargeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MobileRecharge.MobileRechargeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileRechargeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileRechargeProjectApplication.class, args);
+	}
+
+}
---
+++ Mobile-Recharge-project/src/test/java/com/MobileRecharge/MobileRechargeproject/MobileRechargeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MobileRecharge.MobileRechargeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileRechargeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
